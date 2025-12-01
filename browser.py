dd85493d1bf5e0b3d35771bc02b5c6e2087340e6
laz <danier123@gmail.com>
2025-12-01 13:43:01 +0900

    chapter 2

--- browser.py
+++ browser.py
@@ -1,5 +1,56 @@
 import socket
 import ssl
+import tkinter
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        self.scroll = 0
+
+        self.window.bind("<Down>", self.scrolldown)
+
+    def draw(self):
+        self.canvas.delete("all")
+
+        for x, y, c in self.display_list:
+            if y > self.scroll + HEIGHT: continue;
+            if y + VSTEP < self.scroll: continue
+
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def load(self, url):
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+
+        cursor_x += HSTEP
+
+        if(cursor_x >= WIDTH - HSTEP):
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+
+    return display_list
 
 class URL:
     def __init__(self, url):
@@ -64,7 +115,8 @@ class URL:
         return body
     
 
-def show(body):
+def lex(body):
+    text = ""
     in_tag = False
 
     for c in body:
@@ -73,13 +125,10 @@ def show(body):
         elif c == ">":
             in_tag = False
         elif not in_tag:
-            print(c, end="")
-
-def load(url):
-    body = url.request()
-    show(body)
-
+            text += c
+    return text
 
 if __name__ == "__main__":
     import sys
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop();
