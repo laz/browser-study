f2841f24747106b5335db3660d05cf2ee1811ede
laz <danier123@gmail.com>
2025-12-07 23:25:58 +0900

    restructure chapter 4

---
+++ chapter4/4.6.py
@@ -0,0 +1,455 @@
+import socket
+import ssl
+import tkinter
+import tkinter.font
+import os
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+
+    def draw(self):
+        self.canvas.delete("all")
+
+        for x, y, c, font, fill in self.display_list:
+            if y > self.scroll + HEIGHT: continue;
+            if y + VSTEP < self.scroll: continue
+
+            self.canvas.create_text(x, y - self.scroll, text=c, font=font, anchor='nw', fill=fill)
+
+    def load(self, input):
+        if input.startswith("view-source:"):
+            url = URL(input[len("view-source:"):])
+            body = url.request()
+            self.display_list = ViewSourceLayout(body).display_list
+        else:
+            url = URL(input)
+            body = url.request()
+            self.nodes = HTMLParser(body).parse()
+            print_tree(self.nodes)
+            self.display_list = Layout(self.nodes).display_list
+
+        self.draw()
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+class Layout:
+    def __init__(self, tree):
+        self.display_list = []
+        self.line = []
+
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+
+        self.recurse(tree)
+        self.flush()
+
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+
+        baseline = self.cursor_y + 1.25 * max_ascent
+
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font, "black"))
+
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+
+        self.cursor_x = HSTEP
+        self.line = []
+
+
+    def word(self, word):
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+        self.line.append((self.cursor_x, word, font))
+        self.cursor_x += w + font.measure(" ")
+        
+    def open_tag(self, tag):
+        if tag == "i":
+            self.style = "italic"
+        elif tag == "b":
+            self.weight = "bold"
+        elif tag == "small":
+            self.size -= 2
+        elif tag == "big":
+            self.size += 4
+        elif tag == "br":
+            self.flush()
+
+    def close_tag(self, tag):
+        if tag == "i":
+            self.style = "roman"
+        elif tag == "b":
+            self.weight = "normal"
+        elif tag == "small":
+            self.size += 2
+        elif tag == "big":
+            self.size -= 4
+        elif tag == "p":
+            self.flush()
+            self.cursor_y += VSTEP
+
+    def recurse(self, tree):
+        if isinstance(tree, Text):
+            for word in tree.text.split():
+                self.word(word)
+        else:
+            self.open_tag(tree.tag)
+            for child in tree.children:
+                self.recurse(child)
+            self.close_tag(tree.tag)
+                
+class ViewSourceLayout:
+    def __init__(self, body):
+        self.display_list = []
+        self.line = []
+
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+        self.fill = "black"
+
+        self.lex(body)
+        
+    # source code는 동적인 렌더링이 없이 응답받은 그대로 그려야 함
+    # = parse line by line
+    def lex(self, body):
+        lines = body.splitlines()
+        if not lines: return 
+
+        # flush
+        for line in lines:
+            res = self.parse_line(line)
+
+            base_font = get_font(self.size, self.weight, self.style)
+            metrics = base_font.metrics()
+
+            baseline = self.cursor_y + 1.25 * metrics["ascent"]
+            y = baseline - base_font.metrics("ascent")
+
+            for type, content in res:
+                if type == "tag":
+                    self.fill = "blue"
+                    self.weight = "normal"
+                else:
+                    self.fill = "black"
+                    self.weight = "bold"
+
+                font = get_font(self.size, self.weight, self.style)
+
+                self.display_list.append((self.cursor_x, y, content, font, self.fill))
+                self.cursor_x += font.measure(content)
+
+            self.cursor_y = baseline + 1.25 * metrics["descent"]
+            self.cursor_x = HSTEP
+
+    def parse_line(self, line):
+        result = []
+        text = ""
+
+        for c in line:
+            if c == "<":
+                if text:
+                    result.append(("text", text))
+                    text = ""
+                text += c
+            elif c == ">":
+                text += c
+                result.append(("tag", text))
+                text = ""
+            else:
+                text += c
+
+        if text: 
+            result.append(("text", text))
+
+        return result
+
+
+class HTMLParser:
+    SELF_CLOSING_TAGS = [
+        "area", "base", "br", "col", "embed", "hr", "img", "input",
+        "link", "meta", "param", "source", "track", "wbr",
+    ]
+
+    HEAD_TAGS = [
+        "base", "basefont", "bgsound", "noscript",
+        "link", "meta", "title", "style", "script",
+    ]
+
+    FORMATTING_TAGS = [
+        "b", "i", "small", "strong",
+    ]
+
+    def __init__(self, body):
+        self.body = body
+        self.unfinished = []
+
+    def get_attributes(self, text):
+        parts = text.split()
+        tag = parts[0].casefold()
+        attributes = {}
+        for attrpair in parts[1:]:
+            if "=" in attrpair:
+                key, value = attrpair.split("=", 1)
+                attributes[key.casefold()] = value
+
+                if len(value) > 2 and value[0] in ["'", "\""]:
+                    value = value[1:-1]
+            else:
+                attributes[attrpair.casefold()] = ""
+        return tag, attributes
+
+    def implicit_tags(self, tag):
+        while True:
+            open_tags = [node.tag for node in self.unfinished]
+
+            if open_tags == [] and tag != "html":
+                self.add_tag("html")
+            elif open_tags == ["html"] and tag not in ["head", "body", "/html"]:
+                if tag in self.HEAD_TAGS:
+                    self.add_tag("head")
+                else:
+                    self.add_tag("body")
+            elif open_tags == ["html", "head"] and tag not in ["/head"] + self.HEAD_TAGS:
+                self.add_tag("/head")
+            else:
+                break
+
+    def mis_nested_formatting_tags(self, tag): 
+        tmp = []
+
+        matching_open_tag_exists = False
+
+        # 상응하는 open tag가 존재하는지 체크
+        for unfinished_tag in self.unfinished:
+            if unfinished_tag.tag == tag:
+                matching_open_tag_exists = True
+                break
+
+        # 없다면 스킵
+        if not matching_open_tag_exists:
+            return 
+
+        # 현재 닫는 태그와 상응하는 open tag를 만날 때 까지 unfinished 스택에서 꺼내서 다시 스택의 top에 자식으로 넣기
+        # 제대로 닫히지 않았던 format tags를 유효한 태그로 만들어 줌
+        while self.unfinished[-1].tag != tag:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+
+            # 제대로 닫히지 않았던 format tags를 다시 열어주기 위해 저장
+            if node.tag in self.FORMATTING_TAGS:
+                tmp.append(node.tag)
+
+        # 현재 태그도 동일하게 처리
+        node = self.unfinished.pop()
+        parent = self.unfinished[-1]
+        parent.children.append(node)
+
+        # format tags 다시 열어주기
+        while len(tmp):
+            tmp_tag = tmp.pop()
+
+            parent = self.unfinished[-1]
+            node = Element(tmp_tag, {}, parent)
+            self.unfinished.append(node)
+            
+
+    def add_text(self, text):
+        if text.isspace(): return 
+        self.implicit_tags(None)
+
+        parent = self.unfinished[-1]
+        node = Text(text, parent)
+        parent.children.append(node)
+
+    def add_tag(self, tag):
+        tag, attributes = self.get_attributes(tag)
+        if tag.startswith("!"): return
+
+        # 현재 태그가 닫는 포맷팅 태그라면 시도
+        if tag.startswith("/") and tag[1:] in self.FORMATTING_TAGS:
+            self.mis_nested_formatting_tags(tag[1:])
+            return 
+
+        self.implicit_tags(tag)
+
+        if tag.startswith("!"): return
+        elif tag.startswith("/"):
+            if len(self.unfinished) == 1: return
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        elif tag in self.SELF_CLOSING_TAGS:
+            parent = self.unfinished[-1]
+            node = Element(tag, attributes, parent)
+            parent.children.append(node)
+        else:
+            parent = self.unfinished[-1] if self.unfinished else None
+            node = Element(tag, attributes, parent)
+            self.unfinished.append(node)
+
+    def finish(self):
+        if not self.unfinished:
+            self.implicit_tags(None)
+
+        while len(self.unfinished) > 1:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        return self.unfinished.pop()
+
+    def parse(self):
+        text = ""
+        in_tag = False
+
+        for c in self.body:
+            if c == "<":
+                in_tag = True
+                if text: self.add_text(text)
+                text = ""
+            elif c == ">":
+                in_tag = False
+                self.add_tag(text)
+                text = ""
+            else:
+                text += c
+
+        if not in_tag and text:
+            self.add_text(text)
+
+        return self.finish()
+
+class Text:
+    def __init__(self, text, parent):
+        self.text = text
+        self.children = []
+        self.parent = parent
+
+    def __repr__(self):
+        return repr(self.text)
+
+class Element:
+    def __init__(self, tag, attributes, parent):
+        self.tag = tag
+        self.attributes = attributes
+        self.children = [] 
+        self.parent = parent
+
+    def __repr__(self):
+        return "<" + self.tag + ">"
+
+class URL:
+    def __init__(self, url):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https", "file"]
+
+        if "/" not in url:
+            url = url + "/"
+        
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
+
+    def request(self):
+        if self.scheme == "file":
+            with open(self.path, "r", encoding="utf-8") as file:
+                return file.read()
+        else:
+            s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP
+            )
+
+            s.connect((self.host, self.port))
+
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=self.host)
+
+            request = "GET {} HTTP/1.0\r\n".format(self.path)
+            request += "Host: {}\r\n".format(self.host)
+            request += "\r\n"
+
+            s.send(request.encode("utf8"))
+
+            response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+            statusline = response.readline()
+            version, status, explanation = statusline.split(" ", 2)
+            
+            response_headers = {}
+            while True:
+                line = response.readline()
+                if line == "\r\n": 
+                    break
+
+                header, value = line.split(":", 1)
+                response_headers[header.casefold()] = value.strip()
+
+            assert "transfer-encoding" not in response_headers
+            assert "content-encoding" not in response_headers
+
+            body = response.read()
+            s.close()
+
+            return body
+    
+def print_tree(node, indent=0):
+    print(" " * indent, node)
+    for child in node.children:
+        print_tree(child, indent + 2)
+    
+FONTS = {}
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
+
+if __name__ == "__main__":
+    import sys
+
+    input = sys.argv[1]
+    Browser().load(input)
+    tkinter.mainloop();
---
+++ chapter4/browser.py
@@ -0,0 +1,321 @@
+import socket
+import ssl
+import tkinter
+import tkinter.font
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+
+    def draw(self):
+        self.canvas.delete("all")
+
+        for x, y, c, font in self.display_list:
+            if y > self.scroll + HEIGHT: continue;
+            if y + VSTEP < self.scroll: continue
+
+            self.canvas.create_text(x, y - self.scroll, text=c, font=font, anchor='nw')
+
+    def load(self, url):
+        body = url.request()
+        self.nodes = HTMLParser(body).parse()
+        self.display_list = Layout(self.nodes).display_list
+        self.draw()
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+class Layout:
+    def __init__(self, tree):
+        self.display_list = []
+        self.line = []
+
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+
+        self.recurse(tree)
+        self.flush()
+
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+
+        baseline = self.cursor_y + 1.25 * max_ascent
+
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+
+        self.cursor_x = HSTEP
+        self.line = []
+
+
+    def word(self, word):
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+        self.line.append((self.cursor_x, word, font))
+        self.cursor_x += w + font.measure(" ")
+        
+    def open_tag(self, tag):
+        if tag == "i":
+            self.style = "italic"
+        elif tag == "b":
+            self.weight = "bold"
+        elif tag == "small":
+            self.size -= 2
+        elif tag == "big":
+            self.size += 4
+        elif tag == "br":
+            self.flush()
+
+    def close_tag(self, tag):
+        if tag == "i":
+            self.style = "roman"
+        elif tag == "b":
+            self.weight = "normal"
+        elif tag == "small":
+            self.size += 2
+        elif tag == "big":
+            self.size -= 4
+        elif tag == "p":
+            self.flush()
+            self.cursor_y += VSTEP
+
+    def recurse(self, tree):
+        if isinstance(tree, Text):
+            for word in tree.text.split():
+                self.word(word)
+        else:
+            self.open_tag(tree.tag)
+            for child in tree.children:
+                self.recurse(child)
+            self.close_tag(tree.tag)
+                
+class HTMLParser:
+    SELF_CLOSING_TAGS = [
+        "area", "base", "br", "col", "embed", "hr", "img", "input",
+        "link", "meta", "param", "source", "track", "wbr",
+    ]
+
+    HEAD_TAGS = [
+        "base", "basefont", "bgsound", "noscript",
+        "link", "meta", "title", "style", "script",
+    ]
+
+    def __init__(self, body):
+        self.body = body
+        self.unfinished = []
+
+    def get_attributes(self, text):
+        parts = text.split()
+        tag = parts[0].casefold()
+        attributes = {}
+        for attrpair in parts[1:]:
+            if "=" in attrpair:
+                key, value = attrpair.split("=", 1)
+                attributes[key.casefold()] = value
+
+                if len(value) > 2 and value[0] in ["'", "\""]:
+                    value = value[1:-1]
+            else:
+                attributes[attrpair.casefold()] = ""
+        return tag, attributes
+    
+    def implicit_tags(self, tag):
+        while True:
+            open_tags = [node.tag for node in self.unfinished]
+
+            if open_tags == [] and tag != "html":
+                self.add_tag("html")
+            elif open_tags == ["html"] and tag not in ["head", "body", "/html"]:
+                if tag in self.HEAD_TAGS:
+                    self.add_tag("head")
+                else:
+                    self.add_tag("body")
+            elif open_tags == ["html", "head"] and tag not in ["/head"] + self.HEAD_TAGS:
+                self.add_tag("/head")
+            else:
+                break
+
+    def add_text(self, text):
+        if text.isspace(): return 
+        self.implicit_tags(None)
+
+        parent = self.unfinished[-1]
+        node = Text(text, parent)
+        parent.children.append(node)
+
+    def add_tag(self, tag):
+        tag, attributes = self.get_attributes(tag)
+        if tag.startswith("!"): return
+        self.implicit_tags(tag)
+
+        if tag.startswith("!"): return
+        elif tag.startswith("/"):
+            if len(self.unfinished) == 1: return
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        elif tag in self.SELF_CLOSING_TAGS:
+            parent = self.unfinished[-1]
+            node = Element(tag, attributes, parent)
+            parent.children.append(node)
+        else:
+            parent = self.unfinished[-1] if self.unfinished else None
+            node = Element(tag, attributes, parent)
+            self.unfinished.append(node)
+
+    def finish(self):
+        if not self.unfinished:
+            self.implicit_tags(None)
+
+        while len(self.unfinished) > 1:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        return self.unfinished.pop()
+
+    def parse(self):
+        text = ""
+        in_tag = False
+
+        for c in self.body:
+            if c == "<":
+                in_tag = True
+                if text: self.add_text(text)
+                text = ""
+            elif c == ">":
+                in_tag = False
+                self.add_tag(text)
+                text = ""
+            else:
+                text += c
+
+        if not in_tag and text:
+            self.add_text(text)
+
+        return self.finish()
+
+class Text:
+    def __init__(self, text, parent):
+        self.text = text
+        self.children = []
+        self.parent = parent
+
+    def __repr__(self):
+        return repr(self.text)
+
+class Element:
+    def __init__(self, tag, attributes, parent):
+        self.tag = tag
+        self.attributes = attributes
+        self.children = [] 
+        self.parent = parent
+
+    def __repr__(self):
+        return "<" + self.tag + ">"
+
+class URL:
+    def __init__(self, url):
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https"]
+
+        if "/" not in url:
+            url = url + "/"
+        
+        self.host, url = url.split("/", 1)
+        self.path = "/" + url
+
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
+
+
+    def request(self):
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+            proto=socket.IPPROTO_TCP
+        )
+
+        s.connect((self.host, self.port))
+
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+
+        request = "GET {} HTTP/1.0\r\n".format(self.path)
+        request += "Host: {}\r\n".format(self.host)
+        request += "\r\n"
+
+        s.send(request.encode("utf8"))
+
+        response = s.makefile("r", encoding="utf8", newline="\r\n")
+
+        statusline = response.readline()
+        version, status, explanation = statusline.split(" ", 2)
+        
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == "\r\n": 
+                break
+
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        body = response.read()
+        s.close()
+
+        return body
+    
+def print_tree(node, indent=0):
+    print(" " * indent, node)
+    for child in node.children:
+        print_tree(child, indent + 2)
+    
+FONTS = {}
+
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
+
+if __name__ == "__main__":
+    import sys
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop();
