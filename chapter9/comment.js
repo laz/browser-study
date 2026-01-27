6aeea148357593cabf66bd2469d0960f5b7f23c6
laz <danier123@gmail.com>
2026-01-27 10:40:35 +0900

    init chapter 9

---
+++ chapter9/comment.js
@@ -0,0 +1,24 @@
+var strong = document.querySelectorAll("strong")[0];
+var allow_submit = true;
+
+function lengthCheck() {
+  var value = this.getAttribute("value");
+  if (value.length > 10) {
+    strong.innerHTML = "Comment too long!";
+  }
+
+  allow_submit = value.length < 10;
+  if (!allow_submit) {
+    //
+  }
+}
+
+var inputs = document.querySelectorAll("input");
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener("keydown", lengthCheck);
+}
+
+var form = document.querySelectorAll("form")[0];
+form.addEventListener("submit", function (e) {
+  if (!allow_submit) e.preventDefault();
+});
